9de23cd24f656ed7d5036cb344663197260f6dec
vio <ultraviolette-22@yandex.ru>
2023-01-31 01:30:20 +0300

    added function start()

--- bot.py
+++ bot.py
@@ -9,3 +9,10 @@ load_dotenv(find_dotenv())
 bot = Bot(token=os.getenv('Token'))
 dp = Dispatcher(bot)
 
+@dp.message_handler(commands='/start')
+async def start(message: types.Message):
+    await message.answer('Hello')
+    
+
+
+executor.start_polling(dp, skip_updates=True)
