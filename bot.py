3617f249f23b2551f2b035f31ef623665b15062a
vio <ultraviolette-22@yandex.ru>
2023-01-31 16:47:53 +0300

    Added new message_handlers: start_bot (changed from start) get_contacts get_work_hours get_training_courses get_trainers_info

--- bot.py
+++ bot.py
@@ -9,10 +9,38 @@ load_dotenv(find_dotenv())
 bot = Bot(token=os.getenv('Token'))
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands='/start')
-async def start(message: types.Message):
-    await message.answer('Hello')
+
+"""Хендлеры для взаимодействия с клиентом
+"""
+
+@dp.message_handler(commands=['start', 'help'])
+async def start_bot(message: types.Message):
+    bot_home = 't.me/small_school_bot'
+    try:
+        await bot.send_message(message.from_user.id, 'Привет')
+        await bot.delete_message()
+    except:
+        await message.reply(f'Пожалуйста напишите боту в ЛС: {bot_home}')
+    
+@dp.message_handler(commands=['Контакты'])
+async def get_contacts(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Адрес школы и телефоны')
     
 
+@dp.message_handler(commands=['Режим работы'])
+async def get_work_hours(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Режим работы вашей школы')
+    
+
+@dp.message_handler(commands=['Учебные группы'])
+async def get_training_courses(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Выберите учебную группу на клавиатуре')
+    
+
+@dp.message_handler(commands=['Преподаватели'])
+async def get_trainers_info(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Вывод информации на экран о преподавателях')
+    
+    
 
 executor.start_polling(dp, skip_updates=True)
